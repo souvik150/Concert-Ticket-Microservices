dbdc7e1725c2644e627cb0fa4f1c583604c1f037
souvik150 <souvikmukherjee150@gmail.com>
2023-08-20 15:50:39 +0530

    feat: auth setup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,29 +1,42 @@
-import express, {Request, Response} from 'express';
-import {body, validationResult} from 'express-validator';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage("Email must be valid"),
-    body('password')
-        .trim()
-        .isLength({min: 4, max:20})
-        .withMessage("Password must be b/w 4 and 20 characters")
+router.post(
+    '/api/users/signup',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be between 4 and 20 characters')
+    ],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
 
-] ,(req : Request, res: Response) => {
-    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
 
-    if(!errors.isEmpty()){
-        return res.status(400).send(errors.array());
+        const { email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        res.status(201).send(user);
     }
+);
 
-    const {email, password} = req.body;
-
-    console.log("Creating an user");
-    res.send({});
-
-});
-
-export {router as signupRouter }
+export { router as signupRouter };
