ed631470e722c699dd729da6759781c4799539ff
souvik150 <souvikmukherjee150@gmail.com>
2023-08-22 15:13:33 +0530

    feat: test init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,32 +1,10 @@
-import express from 'express';
-import 'express-async-errors';
-import {json} from 'body-parser';
 import mongoose from 'mongoose';
-
-import {currentuserRouter} from "./routes/current-user";
-import {signinRouter} from "./routes/signin";
-import {signupRouter} from "./routes/signup";
-import {signoutRouter} from "./routes/signout";
-
-import {errorHandler} from "./middleware/error-handler";
-import {NotFoundError} from "./errors/not-found-error";
-
-const app = express();
-app.use(json());
-
-
-
-app.use(currentuserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-app.all('*', async () => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler);
+import app from './app';
 
 const start = async () => {
+    if (!process.env.JWT_KEY){
+        throw new Error("JWT_SECRET not defined")
+    }
     try{
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
         console.log("Connected to MongoDB")
