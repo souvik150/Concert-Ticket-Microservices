1ae25075c7800938cd683bf2fcbfd21d7f847282
souvik150 <souvikmukherjee150@gmail.com>
2023-08-22 16:21:48 +0530

    feat: client complete

---
+++ auth/src/test/__test__/current-user.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import app from '../../app';
+
+it('responds with details about the current user', async () => {
+    const authResponse = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@gmail.com",
+            password: "password"
+        })
+        .expect(201);
+
+    const cookie = authResponse.get('Set-Cookie');
+
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .set('Cookie', cookie)
+        .send()
+        .expect(200);
+
+    expect(response.body.currentUser.email).toEqual("test@gmail.com");
+
+});
+
+it('responds with null if not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200);
+
+    expect(response.body.currentUser).toEqual(null);
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,18 @@
-import {MongoMemoryServer} from "mongodb-memory-server";
-import mongoose from "mongoose";
-import app from "../app";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import app from '../app';
+import request from "supertest";
 
 let mongo: any;
+
+declare global {
+    var signin: () => Promise<string[]>;
+}
+
 beforeAll(async () => {
-    mongo = await MongoMemoryServer.create();
+    process.env.JWT_KEY = 'asdfasdf';
+
+    const mongo = await MongoMemoryServer.create();
     const mongoUri = mongo.getUri();
 
     await mongoose.connect(mongoUri, {});
@@ -12,10 +20,12 @@ beforeAll(async () => {
 
 beforeEach(async () => {
     const collections = await mongoose.connection.db.collections();
-    for (let collection of collections){
+
+    for (let collection of collections) {
         await collection.deleteMany({});
     }
 });
+
 afterAll(async () => {
     if (mongo) {
         await mongo.stop();
@@ -23,3 +33,20 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
+
+global.signin = async () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+}
