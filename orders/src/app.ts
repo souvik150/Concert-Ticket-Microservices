8272e7c6d59e7e82469112d1685c5ed960d5505f
souvik150 <souvikmukherjee150@gmail.com>
2023-08-24 14:46:18 +0530

    feat: orders service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,33 +1,32 @@
 import express from 'express';
 import 'express-async-errors';
-import {json} from 'body-parser';
+import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import {currentUser, errorHandler, NotFoundError} from "@souvik150/tickets-common";
-import {createTicketRouter} from "./routes/new";
-import {showTicketRouter} from "./routes/show";
-import {indexTicketRouter} from "./routes/index";
-import {updateTicketRouter} from "./routes/update";
+import { errorHandler, NotFoundError, currentUser } from '@souvik150/tickets-common';
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed: false,
-        secure: process.env.NODE_ENV !== 'test'
-    })
-)
-
-
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
 
-app.all('*', async () => {
-    throw new NotFoundError()
-})
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
