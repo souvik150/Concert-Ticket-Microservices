d0eb53df95bbac8eabd50039194044638f18a419
souvik150 <souvikmukherjee150@gmail.com>
2023-08-24 22:01:37 +0530

    fix: updates ticket restrict if ticket locked

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -29,6 +29,10 @@ router.put(
             throw new NotFoundError();
         }
 
+        if(ticket.orderId){
+            throw new Error('Cannot edit a reserved ticket');
+        }
+
         if (ticket.userId !== req.currentUser!.id) {
             throw new NotAuthorizedError();
         }
