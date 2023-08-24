edab843c161f1727d645392d4452fcec80b3ef4a
souvik150 <souvikmukherjee150@gmail.com>
2023-08-24 18:20:07 +0530

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -3,6 +3,7 @@ import {Subjects} from "./subjects";
 export interface OrderCancelledEvent {
     subject: Subjects.OrderCancelled;
     data: {
+        version: number;
         id: string;
         ticket: {
             id: string;
