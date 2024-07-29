c70403b017af6dec399700952d634567e33ced65
CodeAnt AI <chinmay@codeant.ai>
2024-07-29 06:17:59 +0000

    changed files

--- client/components/header.js
+++ client/components/header.js
@@ -4,7 +4,7 @@ export default ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' }
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
